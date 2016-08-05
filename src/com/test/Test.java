9c64865137bb94515daa48c23522679280606ed6
yangyunying <1259234107@qq.com>
2016-08-05 20:35:37 +0800

    Update Test.java

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -1,9 +1,9 @@
 package com.test;
 
 public class Test {
-
+	//2342342342
 	public static void main(String[] args) {
-		System.out.println("我是一只小小小鸟！");
-		System.out.println("飞不高！");
+		System.out.println("鎴戞槸涓�鍙皬灏忓皬楦燂紒");
+		System.out.println("椋炰笉楂橈紒");
 	}
 }
